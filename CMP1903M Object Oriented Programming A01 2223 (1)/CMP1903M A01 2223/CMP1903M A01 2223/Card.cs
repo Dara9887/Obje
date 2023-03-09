9b9a25e622da235c7b27d6fe4faf5dcb32468c39
Dara9887 <127151721+Dara9887@users.noreply.github.com>
2023-03-09 00:25:16 +0000

    Add files via upload

--- CMP1903M Object Oriented Programming A01 2223 (1)/CMP1903M A01 2223/CMP1903M A01 2223/Card.cs
+++ CMP1903M Object Oriented Programming A01 2223 (1)/CMP1903M A01 2223/CMP1903M A01 2223/Card.cs
@@ -1,18 +1,72 @@
-﻿using System;
+﻿us ing System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace CMP1903M_A01_2223
 {
-    class Card
+    public enum Suit
     {
-        //Base for the Card class.
-        //Value: numbers 1 - 13
-        //Suit: numbers 1 - 4
-        //The 'set' methods for these properties could have some validation
-        public int Value { get; set; }
-        public int Suit { get; set; }
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
+    }
+
+    public enum Rank
+    {
+        Ace = 1,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+
+    public class Card
+    {
+        public Suit Suit { get; set; }
+        public Rank Rank { get; set; }
+
+        public Card(Suit suit, Rank rank)
+        {
+            Suit = suit;
+            Rank = rank;
+        }
+
+        public override string ToString()
+        {
+            string valueString;
+            switch (Rank)
+            {
+                case Rank.Ace:
+                    valueString = "Ace";
+                    break;
+                case Rank.Jack:
+                    valueString = "Jack";
+                    break;
+                case Rank.Queen:
+                    valueString = "Queen";
+                    break;
+                case Rank.King:
+                    valueString = "King";
+                    break;
+                default:
+                    valueString = ((int)Rank).ToString();
+                    break;
+            }
+
+            return $"{valueString} of {Suit}";
+        }
     }
 }
--- CMP1903M Object Oriented Programming A01 2223 (1)/CMP1903M A01 2223/CMP1903M A01 2223/Pack.cs
+++ CMP1903M Object Oriented Programming A01 2223 (1)/CMP1903M A01 2223/CMP1903M A01 2223/Pack.cs
@@ -4,30 +4,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
+
 namespace CMP1903M_A01_2223
 {
     class Pack
     {
-        List<Card> pack;
+        private List<Card> pack;
 
         public Pack()
         {
-            //Initialise the card pack here
+            // Initialize the card pack here
+            this.pack = new List<Card>();
+
+            // Add all the cards to the pack
+            foreach (suit Suit in Enum.GetValues(typeof(suit)))
+            {
+                foreach (Rank rank in Enum.GetValues(typeof(Rank)))
+                { 
+                    this.pack.Add(new Card(Suit, rank));
+                }
+            }
         }
 
-        public static bool shuffleCardPack(int typeOfShuffle)
+        public bool ShuffleCardPack(int typeOfShuffle)
         {
-            //Shuffles the pack based on the type of shuffle
+            // Shuffles the pack based on the type of shuffle
+            switch (typeOfShuffle)
+            {
+                case 1:
+                    // Perform a simple shuffle
+                    this.pack = this.pack.OrderBy(x => Guid.NewGuid()).ToList();
+                    break;
 
-        }
-        public static Card deal()
-        {
-            //Deals one card
+                case 2:
+                    // Perform a more complex shuffle
+                    // ...
+                    break;
 
+                default:
+                    // Invalid shuffle type
+                    return false;
+            }
+
+            return true;
         }
-        public static List<Card> dealCard(int amount)
+
+        public Card Deal()
         {
-            //Deals the number of cards specified by 'amount'
+            // Deals one card
+            if (this.pack.Count == 0)
+            {
+                throw new InvalidOperationException("The card pack is empty");
+            }
+
+            Card card = this.pack[0];
+            this.pack.RemoveAt(0);
+            return card;
+        }
+
+        public List<Card> DealCard(int amount)
+        {
+            // Deals the number of cards specified by 'amount'
+            if (amount > this.pack.Count)
+            {
+                throw new InvalidOperationException("Not enough cards in the pack");
+            }
+
+            List<Card> cards = this.pack.Take(amount).ToList();
+            this.pack.RemoveRange(0, amount);
+            return cards;
         }
     }
 }
